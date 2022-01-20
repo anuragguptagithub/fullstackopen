ebd98a4115c74622e2fc7a28acb123e360b24c79
anuragguptagithub <anurag.gupta@home24.de>
2022-01-20 02:07:02 +0100

    separated model

--- part4/blog-backend/index.js
+++ part4/blog-backend/index.js
@@ -1,21 +1,11 @@
 const http = require('http')
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-require('dotenv').config()
+const Blog = require('./models/blog')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
