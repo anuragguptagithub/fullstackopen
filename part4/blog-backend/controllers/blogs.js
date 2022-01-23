b044593d71795d098903cf5f9493cac1c14c377f
anuragguptagithub <anurag.gupta@home24.de>
2022-01-23 19:16:58 +0100

    Test cases added

--- part4/blog-backend/controllers/blogs.js
+++ part4/blog-backend/controllers/blogs.js
@@ -1,22 +1,31 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs.map(blog => blog.toJSON()))
+
   })
   
-  blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
+    result = await blog.save();
+    response.status(201).json(result)
+
   })
+
+blogsRouter.delete('/:id', async (request, response) => {
+    
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+
+  })
+
+blogsRouter.put('/:id', async (request, response) => {
+
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, request.body, { new: true })
+    response.status(201).json(updatedBlog)
+})
+  
 
 module.exports = blogsRouter
